ce919676da3c6af743ee897814b0fe699ead5249
Erikson <chris.s.erickson@gmail.com>
2022-10-24 18:21:09 -0400

    indexeddb getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,20 @@ export const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+// Get function from indexedDB
+export const getDb = async () => {
+  console.log("GET from the jate database");
+// create connection to database
+  const jateDB = await openDB("jate", 1):
+    // create new transaction with privileges
+  const tx = jateDB.transaction("jate", "readonly");
+  // open up desired object storage
+  const store = tx.objectStore("jate");
+  // use getAll()
+  const request = store.getAll();
+  // get confirmation of the request
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+}
 initdb();
