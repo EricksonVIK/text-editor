98737b25d9e845179ee2a84295e1e238b242b281
Erikson <chris.s.erickson@gmail.com>
2022-10-25 20:21:10 -0400

    readme edits

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Text Editor Starter Code
+# J.A.T.E. - Just Another Text Editor
+
+![](https://img.shields.io/github/languages/count/EricksonVIK/text-editor)
+
+![](https://img.shields.io/github/languages/top/EricksonVIK/text-editor)
+
+## Description
+
+A Progressive Web App (PWA) that incorporates indexedDB to save and display data. The app can be installed and utilized without internet.
+
+## Table of contents
+- [Deployed APP](#deployed-app)
+- [Usage](#usage)
+- [Screenshot](#screenshot)
+- [Contact Infomation](#contact)
+- [License](#license)
+
+## Deployed App
+
+## Usage
+
+## Screenshot
+
+![](screenshots/JATE%20screenshot.png)
+![](screenshots/JATE%20Service%20Workder%20screenshot.png)
+![](screenshots/JATE%20IndexedDB%20screenshot.png)
+![](screenshots/JATE%20Manifest%20screenshot.png)
+
+## Usage
+
+## Contact
+
+https://github.com/EricksonVIK </br>
+[ericksonvik76@gmail.com](mailto:ericksonvik76@gmail.com) </br>
+
+## License
+
+![badmath](https://img.shields.io/github/license/EricksonVIK/text-editor)
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,7 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// An event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     // Store the triggered events
 	window.deferredPrompt = event;
@@ -11,7 +11,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 	butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// A click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
 
@@ -28,8 +28,9 @@ butInstall.addEventListener('click', async () => {
 	butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// A handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     // Clear prompt
-	window.deferredPrompt = null;
+    window.deferredPrompt = null;
+    console.log("🌐Text-Editor Installed!🌐")
 });
